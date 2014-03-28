3431d5f17b45c1423d1d96ec4709b6656412e015
manishmore <manish.more64@gmail.com>
2014-03-28 23:09:48 +0530

    administrator

---
+++ sql/install.mysql.utf8.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `#__studentreg_member` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+`certificate_number` INT(11)  NOT NULL ,
+`course` VARCHAR(255)  NOT NULL ,
+`college` VARCHAR(255)  NOT NULL ,
+`date` DATETIME(11)  NOT NULL ,
+`grade` VARCHAR(255)  NOT NULL ,
+`image` VARCHAR(255)  NOT NULL ,
+`name` VARCHAR(255)  NOT NULL ,
+PRIMARY KEY (`id`)
+) DEFAULT COLLATE=utf8_general_ci;
+
---
+++ sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `#__studentreg_member`;
